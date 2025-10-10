46b3a6f12e3255b4c3f9a7e348466b7282f765f6
이종서 <tony2428@naver.com>
2025-10-10 12:00:00 +0900

    10월31일 커밋

--- commit_script.ps1
+++ commit_script.ps1
@@ -1,6 +1,6 @@
 cd "D:\2025-2학기\JAVA심화\git\BitBot"
 
-for ($i = 1; $i -le 5; $i++) {
+for ($i = 1; $i -le 7; $i++) {
     # README.md에 'a' 추가
     Add-Content -Path "README.md" -Value "a"
     
@@ -8,8 +8,8 @@ for ($i = 1; $i -le 5; $i++) {
     git add .
     
     # 환경 변수 설정 및 커밋
-    $env:GIT_AUTHOR_DATE = "2025-10-17T12:00:00"
-    $env:GIT_COMMITTER_DATE = "2025-10-17T12:00:00"
+    $env:GIT_AUTHOR_DATE = "2025-10-10T12:00:00"
+    $env:GIT_COMMITTER_DATE = "2025-10-10T12:00:00"
     git commit -m "10월31일 커밋"
     
     Write-Host "커밋 $i 완료"
